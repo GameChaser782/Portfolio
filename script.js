42030c64a54bc53b57a10d518c39d8ff9655a0dd
Harshit Upadhyay <gamechaseriitjee2020@gmail.com>
2022-05-10 18:27:05 +0530

    big changes

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-function myFunction() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
- }
+var darkSwitch = document.getElementById("darkSwitch");
+window.addEventListener("load", function () {
+   if (darkSwitch) {
+     initTheme();
+     darkSwitch.addEventListener("change", function () {
+       resetTheme();
+     });
+   }
+ })
+var darkSwitch=document.getElementById("darkSwitch");window.addEventListener("load",(function(){if(darkSwitch){initTheme();darkSwitch.addEventListener("change",(function(){resetTheme()}))}}));function initTheme(){var darkThemeSelected=localStorage.getItem("darkSwitch")!==null&&localStorage.getItem("darkSwitch")==="dark";darkSwitch.checked=darkThemeSelected;darkThemeSelected?document.body.setAttribute("data-theme","dark"):document.body.removeAttribute("data-theme")}function resetTheme(){if(darkSwitch.checked){document.body.setAttribute("data-theme","dark");localStorage.setItem("darkSwitch","dark")}else{document.body.removeAttribute("data-theme");localStorage.removeItem("darkSwitch")}}
