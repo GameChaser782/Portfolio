a6173c59f48ead81dc0014323d8503aa4caa83c5
Harshit Upadhyay <79763365+GameChaser782@users.noreply.github.com>
2024-07-03 22:37:13 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-var darkSwitch = document.getElementById("darkSwitch");
-window.addEventListener("load", function () {
-   if (darkSwitch) {
-     initTheme();
-     darkSwitch.addEventListener("change", function () {
-       resetTheme();
-     });
-   }
- })
-var darkSwitch=document.getElementById("darkSwitch");window.addEventListener("load",(function(){if(darkSwitch){initTheme();darkSwitch.addEventListener("change",(function(){resetTheme()}))}}));function initTheme(){var darkThemeSelected=localStorage.getItem("darkSwitch")!==null&&localStorage.getItem("darkSwitch")==="dark";darkSwitch.checked=darkThemeSelected;darkThemeSelected?document.body.setAttribute("data-theme","dark"):document.body.removeAttribute("data-theme")}function resetTheme(){if(darkSwitch.checked){document.body.setAttribute("data-theme","dark");localStorage.setItem("darkSwitch","dark")}else{document.body.removeAttribute("data-theme");localStorage.removeItem("darkSwitch")}}
+const darkSwitch = document.getElementById("darkSwitch");
+const body = document.body;
+
+darkSwitch.addEventListener("change", () => {
+  if (darkSwitch.checked) {
+    body.dataset.theme = "dark";
+  } else {
+    body.dataset.theme = "";
+  }
+});
